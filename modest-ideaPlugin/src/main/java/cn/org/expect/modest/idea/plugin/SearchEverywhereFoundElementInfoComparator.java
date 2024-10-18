c3e9f4fa26549029c92b1f9fdafd6d7571f6f82a
jeremy8551 <jeremy8551@gmail.com>
2024-10-18 23:39:29 +0800

    优化1

--- modest-ideaPlugin/src/main/java/cn/org/expect/modest/idea/plugin/SearchEverywhereFoundElementInfoComparator.java
+++ modest-ideaPlugin/src/main/java/cn/org/expect/modest/idea/plugin/SearchEverywhereFoundElementInfoComparator.java
@@ -8,6 +8,30 @@ public class SearchEverywhereFoundElementInfoComparator implements Comparator<Se
 
     @Override
     public int compare(SearchEverywhereFoundElementInfo o1, SearchEverywhereFoundElementInfo o2) {
-        return 0;
+        if (o1 == null && o1 == null) {
+            return 0;
+        }
+        if (o1 == null) {
+            return -1;
+        }
+        if (o2 == null) {
+            return 1;
+        }
+
+        String n1 = o1.getElement().getClass().getName();
+        String n2 = o2.getElement().getClass().getName();
+        int nv = n1.compareTo(n2);
+        if (nv != 0) {
+            return nv;
+        }
+
+        String c1 = o1.getContributor().getClass().getName();
+        String c2 = o2.getContributor().getClass().getName();
+        int cv = c1.compareTo(c2);
+        if (cv != 0) {
+            return cv;
+        }
+
+        return o1.getPriority() - o2.getPriority();
     }
 }
