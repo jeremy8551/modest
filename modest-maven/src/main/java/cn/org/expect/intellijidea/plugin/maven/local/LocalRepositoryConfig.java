90f240211d47d65e5ae89b7837da77abb7cd3735
jeremy8551 <jeremy8551@gmail.com>
2024-11-08 12:29:02 +0800

    优化1

--- modest-maven/src/main/java/cn/org/expect/intellijidea/plugin/maven/local/LocalRepositoryConfig.java
+++ modest-maven/src/main/java/cn/org/expect/intellijidea/plugin/maven/local/LocalRepositoryConfig.java
@@ -2,16 +2,12 @@ package cn.org.expect.intellijidea.plugin.maven.local;
 
 import java.io.File;
 
-import cn.org.expect.util.Ensure;
+import cn.org.expect.util.ClassUtils;
 import cn.org.expect.util.Settings;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
-import org.jetbrains.idea.maven.project.MavenGeneralSettings;
-import org.jetbrains.idea.maven.project.MavenProjectsManager;
 
-public class LocalRepositoryConfig implements MavenGeneralSettings.Listener, Disposable {
+public class LocalRepositoryConfig {
     private static final Logger log = Logger.getInstance(LocalRepositoryConfig.class);
 
     private static volatile LocalRepositoryConfig INSTANCE;
@@ -27,66 +23,35 @@ public class LocalRepositoryConfig implements MavenGeneralSettings.Listener, Dis
         return INSTANCE;
     }
 
-    private File repository;
-
-    private final AnActionEvent event;
+    protected static volatile File REPOSITORY;
 
     protected LocalRepositoryConfig(AnActionEvent event) {
-        this.event = Ensure.notNull(event);
-
-        if (this.loadIdeaSettings(this.event)) {
-            return;
+        // 如果idea中安装了 Maven 插件
+        if (ClassUtils.forName("org.jetbrains.idea.maven.project.MavenProjectsManager") != null) {
+            IdeaMavenPluginListener.start(event);
         }
 
         // 如果 idea 中没有安装 maven 插件
-        if (this.autoFind()) {
-            return;
+        if (REPOSITORY == null) {
+            this.findLocalRepository();
         }
 
-        log.warn("No Maven local repository found!");
+        if (REPOSITORY == null) {
+            log.warn("No Maven local repository found!");
+        }
     }
 
-    protected boolean autoFind() {
+    protected void findLocalRepository() {
         File m2 = new File(Settings.getUserHome(), ".m2");
         if (m2.exists() && m2.isDirectory()) {
             File repository = new File(m2, "repository");
             if (repository.exists() && repository.isDirectory()) {
-                this.repository = repository;
-                return true;
+                LocalRepositoryConfig.REPOSITORY = repository;
             }
         }
-        return false;
-    }
-
-    protected boolean loadIdeaSettings(AnActionEvent event) {
-        Project project = event.getProject();
-        MavenProjectsManager manager = MavenProjectsManager.getInstance(Ensure.notNull(project));
-        if (manager != null) {
-            String filepath = manager.getLocalRepository().getAbsolutePath();
-//            System.out.println("filepath: " + filepath);
-
-            MavenGeneralSettings settings = manager.getGeneralSettings();
-            settings.addListener(this, this);
-
-//            System.out.println(settings.getMavenHome());
-//            System.out.println("Maven Local Repository Path: " + settings.getLocalRepository());
-
-            this.repository = new File(filepath); // 获取 Maven 本地仓库路径
-            return true;
-        }
-        return false;
     }
 
     public File getRepository() {
-        return this.repository;
-    }
-
-    @Override
-    public void changed() {
-        this.loadIdeaSettings(this.event);
-    }
-
-    @Override
-    public void dispose() {
+        return REPOSITORY;
     }
 }
