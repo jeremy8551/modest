28eaa40c6b348f21feadb774a4ca462fd904ffa5
jeremy8551 <jeremy8551@gmail.com>
2024-11-13 06:19:15 +0800

    优化1

--- modest-maven/src/main/java/cn/org/expect/maven/intellij/idea/MavenPluginFactory.java
+++ modest-maven/src/main/java/cn/org/expect/maven/intellij/idea/MavenPluginFactory.java
@@ -1,6 +1,7 @@
 package cn.org.expect.maven.intellij.idea;
 
 import cn.org.expect.log.LogFactory;
+import cn.org.expect.maven.intellij.idea.log.IdeaLogBuilder;
 import cn.org.expect.maven.search.MavenSearchMessage;
 import com.intellij.CommonBundle;
 import com.intellij.ide.actions.searcheverywhere.SearchEverywhereContributor;
@@ -13,6 +14,8 @@ public class MavenPluginFactory implements SearchEverywhereContributorFactory<Ob
     public @NotNull SearchEverywhereContributor<Object> createContributor(@NotNull AnActionEvent event) {
         if (Boolean.parseBoolean(System.getProperty("idea.is.internal"))) { // 判断是否是开发测试阶段
             LogFactory.set("sout+:debug");
+        } else {
+            LogFactory.getContext().setBuilder(new IdeaLogBuilder());
         }
 
         MavenSearchMessage.setChineseCondition((key) -> "取消".equals(CommonBundle.getCancelButtonText()));
