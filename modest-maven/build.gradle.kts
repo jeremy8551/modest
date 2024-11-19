2a64e1d841978be0ffafa60b2dbdd1d7a06dfade
jeremy8551 <jeremy8551@gmail.com>
2024-11-19 10:40:14 +0800

    优化1

--- modest-maven/build.gradle.kts
+++ modest-maven/build.gradle.kts
@@ -54,8 +54,8 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("232") // TODO
-        untilBuild.set("242.*")
+//        sinceBuild.set("232") // TODO
+//        untilBuild.set("242.*")
     }
 
     signPlugin {
